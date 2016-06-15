9d7fbb795202649de3f17ca084b8f79b79f2b69b
theodoregoetz <theodore.goetz@gmail.com>
2016-06-15 07:25:24 -0700

    more development on PID controller

--- sandbox/remotus_immobilous/plot_telemetry.py
+++ sandbox/remotus_immobilous/plot_telemetry.py
@@ -4,7 +4,7 @@ from telemetry import Telemetry
 from matplotlib import pyplot
 
 
-h5file = tables.open_file('science1b_telemetry.h5','r')
+h5file = tables.open_file('telemetry.h5','r')
 table = h5file.root.telemetry.readout
 
 t = np.linspace(0,60,60*10)
@@ -21,4 +21,23 @@ for i,l,c in zip([2,3,4],['pitch','heading','roll'],['con_yaw','con_pitch','con_
     axr.plot(t[:a],[x[c] for x in table.iterrows()][:a], color='g')
     ax[i].set_title(l+' / '+c)
 
+fig,ax = pyplot.subplots(2)
+
+pitch = np.array([x['pitch'] for x in table.iterrows()][:a])
+dpitch = np.abs(pitch[1:] - pitch[:-1])
+dt = t[:a][1:] - t[:a][:-1]
+ddpitch = dpitch[1:] - dpitch[:-1]
+ddt = dt[1:] - dt[:-1]
+
+ax[0].plot(t[:a][:-1],dpitch/dt)
+ax[1].plot(t[:a][:-2],ddpitch/ddt)
+
 pyplot.show()
+
+
+#t, dpitch/dt
+#3.57, 55
+#5.6, 389
+#
+#ddpitch/ddt = 165 deg / s^2
+#        = 2.88 rad / s^2
--- sandbox/remotus_immobilous/telemetry.py
+++ sandbox/remotus_immobilous/telemetry.py
@@ -4,6 +4,7 @@ import krpc
 
 
 class Telemetry(tables.IsDescription):
+    time            = tables.Float64Col()
     altitude        = tables.Float64Col()
     vertical_speed  = tables.Float64Col()
     speed           = tables.Float64Col()
@@ -22,7 +23,6 @@ def record(linkup):
     surface_flight = vessel.flight(body.reference_frame)
     con = vessel.control
 
-
     altitude = linkup.add_stream(getattr, surface_flight, 'mean_altitude')
     vertical_speed = linkup.add_stream(getattr, surface_flight, 'vertical_speed')
     speed = linkup.add_stream(getattr, surface_flight, 'speed')
@@ -36,29 +36,28 @@ def record(linkup):
     con_yaw = linkup.add_stream(getattr, con, 'yaw')
     con_roll = linkup.add_stream(getattr, con, 'roll')
 
-    h5file = tables.open_file('science1b_telemetry.h5', mode='w', title='data')
-
-    group = h5file.create_group('/', 'telemetry', 'Flight information')
-    table = h5file.create_table(group, 'readout', Telemetry, 'Telemetry information')
-
-    tel = table.row
-    count = 60*10
-    while count > 0:
-        tel['altitude'] = altitude()
-        tel['vertical_speed'] = vertical_speed()
-        tel['speed'] = speed()
-        tel['pitch'] = pitch()
-        tel['heading'] = heading()
-        tel['roll'] = roll()
-        tel['con_pitch'] = con_pitch()
-        tel['con_yaw'] = con_yaw()
-        tel['con_roll'] = con_roll()
-        tel.append()
+    try:
+        h5file = tables.open_file('telemetry.h5', mode='w', title='data')
+        group = h5file.create_group('/', 'telemetry', 'Flight information')
+        table = h5file.create_table(group, 'readout', Telemetry, 'Telemetry information')
+        tel = table.row
+        while True:
+            tel['time'] = ksc.ut
+            tel['altitude'] = altitude()
+            tel['vertical_speed'] = vertical_speed()
+            tel['speed'] = speed()
+            tel['pitch'] = pitch()
+            tel['heading'] = heading()
+            tel['roll'] = roll()
+            tel['con_pitch'] = con_pitch()
+            tel['con_yaw'] = con_yaw()
+            tel['con_roll'] = con_roll()
+            tel.append()
+            count -= 1
+            time.sleep(0.01)
+    finally:
         table.flush()
-        count -= 1
-        time.sleep(0.1)
-
-    h5file.close()
+        h5file.close()
 
 if __name__ == '__main__':
     linkup = krpc.connect('192.168.1.2', name='telemetry')
